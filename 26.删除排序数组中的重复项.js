0adb117a4db9aac166867b6e5699685f64cccd06
Hughes.Cai <845299100@qq.com>
2023-01-18 22:10:22 +0800

    26

--- 26.删除排序数组中的重复项.js
+++ 26.删除排序数组中的重复项.js
@@ -11,9 +11,9 @@
  */
 var removeDuplicates = function(nums) {
   if (nums.length === 0) return 0;
-  /**         i
-   *  0,1,2,3,4,1,2,1,3,0
-   *                    y
+  /**           i       
+   *  0,1,2,3,4,2,2,3,3,4
+   *                    y                
    */
   let i = 1;
   let y = 1;
